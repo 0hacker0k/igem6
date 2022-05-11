a79e444e827a3dbda1f1e0e86773f1e6816f6e8e
0hacker0k <55192104+0hacker0k@users.noreply.github.com>
2022-05-11 17:52:38 +0800

    bootstrap header & footer

--- webgame.php
+++ webgame.php
@@ -1,28 +1,38 @@
 <!doctype html> 
 <html lang="zh-tw"> 
 <head>
-    <?php include "sub/head.html"?>
-    <?php include "sub/gamehead.html"?>
+    <?php include "sub/config.php"?>
+    <?php include "sub/head.php"?>
+    <?php include "sub/gamehead.php"?>
     <meta charset="UTF-8" />
     <title>igem6 web game</title>
     <!-- <script src="phaser-3.55.2/dist/phaser.js"></script> -->
     
-    <style type="text/css">
+    <!-- <style type="text/css">
         body {
             margin: 10%;
         }
-    </style>
+    </style> -->
 </head>
 <body>
+    <?php include "sub/header.php"?>
     <!-- <div style="text-align: center;">
         <div>hiuuuu</div>
     </div> -->
-    <select id="language_set" onChange="changeLang(this.options[this.selectedIndex].value)">
-        <!-- <option value="#">please choose your language</option> -->
-        <option value="en">English</option>
-        <option value="zh-tw">繁體中文</option>
-        <option value="zh-cn">簡體中文</option>
-    </select>
+    <br>
+    <div class="maindiv">
+        <div class="centerdiv">
+            <div>
+                <select id="language_set" onChange="changeLang(this.options[this.selectedIndex].value)">
+                    <option value="en">English</option>
+                    <option value="zh-tw">繁體中文</option>
+                    <option value="zh-cn">簡體中文</option>
+                </select>
+            </div>
+            <div id="game"></div>
+        </div>
+    </div>
+    <?php include "sub/footer.php"?>
 <!-- <button onclick="load()">123123132</button> -->
 </body>
 </html>
