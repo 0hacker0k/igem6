e7a9ae6a6cae97e78d6f0bd7d5d4e375ae9aa475
ijh1221 <justinj1221@gmail.com>
2022-05-11 18:00:33 +0800

    testijh

--- testijh/wiki.js
+++ testijh/wiki.js
@@ -1,2 +1,27 @@
-var myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello world!';
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction(id) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                    openDropdown.classList.remove('show');
+            }
+    }
+    document.getElementById(id).classList.toggle("show");
+  }
+  
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+            var dropdowns = document.getElementsByClassName("dropdown-content");
+            var i;
+            for (i = 0; i < dropdowns.length; i++) {
+                    var openDropdown = dropdowns[i];
+                    if (openDropdown.classList.contains('show')) {
+                            openDropdown.classList.remove('show');
+                    }
+            }
+    }
+}
