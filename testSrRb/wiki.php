a1d120f6e1a5eebc855771d4c03fd6b240d8e00a
SrrrrrrRb <cheryl20010902@gmail.com>
2022-05-15 15:23:03 +0800

    imember intro and p

--- testSrRb/wiki.php
+++ testSrRb/wiki.php
@@ -17,10 +17,55 @@
 
   </div> 
 
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+
+
+  <div class=intro>
+    <h1>Introduction</h1>
+    <p>CCU_Taiwan is established in the year of 2017. Our team consists of twenty-one passionate students. We are a multidisciplinary team as our team members have different professional background from five departments.</p>
+
+  </div>
+
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+
+  <div class=member_card>
+        <!-- <div class="single" style="z-index: 2;"> -->
+            <!-- <div class="circle"></div> -->
+            <div class="inner">
+                <h3>Cringe</h3>
+                <div class="image">
+                    <img src="./human/cringe.png" alt="">
+                </div>
+                <div class="social">
+                    <button type="button" class="btn btn-md" data-toggle="modal" data-target="#myModal1" style="background-color: #FFF985;">
+                        click to see more
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+  
 
 
 
-  <!-- <?php include "../sub/footer.php"?> -->
+
+  </div>
+
+
+
+  <?php include "../sub/footer.php"?>
   
 
      
