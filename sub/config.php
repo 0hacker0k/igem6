f39e1563f979e625d6625e7ea1c18d0dcda601ec
0hacker0k <55192104+0hacker0k@users.noreply.github.com>
2022-05-18 18:47:38 +0800

    debug

--- sub/config.php
+++ sub/config.php
@@ -1,5 +1,7 @@
 <?php
 define("root",substr(dirname(__FILE__,2),strlen($_SERVER['DOCUMENT_ROOT'])));
+echo dirname(__FILE__,2);
+echo dirname($_SERVER['DOCUMENT_ROOT']);
 define("cssfile", root."/css");
 define("jsfile", root."/js");
 define("webgamejsfile", root."/webgamejs");
