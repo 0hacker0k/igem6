91d633db04c1bd8984c835412e6e439e55faf7d9
0hacker0k <55192104+0hacker0k@users.noreply.github.com>
2022-05-22 16:33:15 +0800

    header debug

--- js/index.js
+++ js/index.js
@@ -4,14 +4,14 @@ function hide_bar(id) {
     document.getElementById(id).classList.remove('show');
 } 
 function show_bar(id) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-            openDropdown.classList.remove('show');
-        }
-    }
+    // var dropdowns = document.getElementsByClassName("dropdown-content");
+    // var i;
+    // for (i = 0; i < dropdowns.length; i++) {
+    //     var openDropdown = dropdowns[i];
+    //     if (openDropdown.classList.contains('show')) {
+    //         openDropdown.classList.remove('show');
+    //     }
+    // }
     document.getElementById(id).classList.toggle("show");
 }
  
