6e4926d19a4de1ccce4b39fe6e8d2f451d341316
0hacker0k <55192104+0hacker0k@users.noreply.github.com>
2022-05-22 20:32:17 +0800

    member page debug

--- sub/popup/popup.php
+++ sub/popup/popup.php
@@ -5,23 +5,23 @@
 
 <!-- Modal -->
 <div class="modal fade" id="popup<?php echo $id;?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel"><?php echo $name;?></h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-        <div class="image">
-        <img src="<?php echo $popup_img;?>" alt="<?php echo $popup_img_alt;?>">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel"><?php echo $name;?></h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <div class="image">
+                    <img src="<?php echo $popup_img;?>" alt="<?php echo $popup_img_alt;?>">
+                </div>
+                <p><?php echo $content;?></p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            </div>
         </div>
-        <p><?php echo $content;?></p>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-      </div>
     </div>
-  </div>
 </div>
