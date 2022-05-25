9958098a891616edc5106f678ab1a5923acdc94f
ijh1221 <justinj1221@gmail.com>
2022-05-25 18:25:40 +0800

    testijh

--- testijh/notebook.php
+++ testijh/notebook.php
@@ -9,12 +9,31 @@
 <body>
     <?php include "../sub/header.php"?>
     
-    <script>
-        var x=1,y=1;
-        document.onclick = function(){
+    <button onclick="switchbtn(1)">1</button>
+    <button onclick="switchbtn(2)">2</button>
+    <button onclick="switchbtn(10)">10</button>
+    <button onclick="switchbtn(20)">20</button>
 
+    <div id="id11"><p>1111111111</p></div>
+    <div id="id12"><p>2222222222</p></div>
+    <div id="id21"><p>3333333333</p></div>
+    <div id="id22"><p>4444444444</p></div>
+    <script>
+        var x=1,y=10;
+        function switchbtn(id){
+            if(id>=10)y=id;
+            else x=id;
+            $("#id"+(y+x).toString()).show();
         }
+        $("#id11").hide();
+        $("#id12").hide();
+        $("#id21").hide();
+        $("#id22").hide();
+        function loadingFunction(){
+      $(".loading").delay(1000).hide();
+    }
     </script>
+
 
     <br>
     <br>
