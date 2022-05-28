ca2245b0ae9a04eb96ce245fec755289c124f368
SrrrrrrRb <cheryl20010902@gmail.com>
2022-05-28 20:00:48 +0800

    notebook 1

---
+++ notebook.php
@@ -0,0 +1,72 @@
+<head>
+  
+  <title>wiki</title>
+  <?php include "./sub/config.php";?>
+  <?php include "./sub/head.php";?>
+  <link rel="stylesheet" media="all" href="<?php echo cssfile;?>/notebook.css">
+  
+</head>
+
+<body>
+  <?php include "./sub/header.php"?>
+
+
+  
+    <!-- <img class="full_size_image" src="<?php echo imgfile;?>/human/fullimage.jpg" alt=""> -->
+
+    
+
+  </div> 
+
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+
+  <div class="out">
+
+    <h1>NOTEBOOK</h1>
+    <hr>  
+
+    <ul>
+      <li>
+        <button type="button">WET LAB</button>
+      </li>
+      
+      <li>
+      <button type="button">DRY LAB</button>
+      </li>
+      
+      <li>
+      <button type="button">HP</button>
+      </li>
+
+    </ul>
+  
+  </div>
+
+
+  
+
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+
+
+
+
+
+  <?php include "./sub/footer.php"?>
+  
+
+     
+    
+        
+      
+    
+</body>
