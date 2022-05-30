5f9726fd93f7e21c70971ff2c1bb5e48b51a23d7
SrrrrrrRb <cheryl20010902@gmail.com>
2022-05-30 16:15:29 +0800

    my notebook is a small catastrophe

--- notebook.php
+++ notebook.php
@@ -8,15 +8,8 @@
 </head>
 
 <body>
+
   <?php include "./sub/header.php"?>
-
-
-  
-    <!-- <img class="full_size_image" src="<?php echo imgfile;?>/human/fullimage.jpg" alt=""> -->
-
-    
-
-  </div> 
 
   <br>
   <br>
@@ -29,7 +22,7 @@
 
       <div class="top">
 
-        <ul>
+          <ul>
             <li>
                 <button type="button" class="toplabel toplabel1">WET LAB</button>
             </li>
@@ -45,85 +38,77 @@
             <li>
                 <button type="button" class="toplabel">TIMELINE</button>
             </li>
+          </ul>
+
+      </div>
+
+      <div class="left">
+          
+        <ul>
+            <li>
+                <button type="button" class="leftlabel leftlabel1">MAY.</button>
+            </li>
+
+            <li>
+                <button type="button" class="leftlabel leftlabel2">JUN.</button>
+            </li>
+
+            <li>
+                <button type="button" class="leftlabel leftlabel3">JUL.</button>
+            </li>
+
+            <li>
+                <button type="button" class="leftlabel leftlabel4">AUG.</button>
+            </li>
+
+            <li>
+                <button type="button" class="leftlabel leftlabel5">SEP.</button>
+            </li>
+
+            <li>
+                <button type="button" class="leftlabel leftlabel6">OCT.</button>
+            </li>
         </ul>
 
       </div>
 
-    <div class="left">
+      <div class="out">
 
-        <ul>
-          <li>
-              <button type="button" class="leftlabel leftlabel1">MAY.</button>
-          </li>
+        <h1>NOTEBOOK</h1>
+        <hr>
 
-          <li>
-              <button type="button" class="leftlabel leftlabel2">JUN.</button>
-          </li>
-
-          <li>
-              <button type="button" class="leftlabel leftlabel3">JUL.</button>
-          </li>
-
-          <li>
-              <button type="button" class="leftlabel leftlabel4">AUG.</button>
-          </li>
-
-          <li>
-              <button type="button" class="leftlabel leftlabel5">SEP.</button>
-          </li>
-
-          <li>
-              <button type="button" class="leftlabel leftlabel6">OCT.</button>
-          </li>
-        </ul>
-
-    </div>
-
-    
-
-
-      
-
-      
-
-
-      <div class="out">  
-
-
-
-      
-      
-
-          
-
-        <div class="mid">
-
-          <h1>NOTEBOOK</h1>
-          <!-- <hr> -->
-            <ul>
-              <li>
-                <button type="button" class="midbutton">WET LAB</button>
-              </li>
+          <div class="mid">
               
-              <li>
-                <button type="button" class="midbutton">DRY LAB</button>
-              </li>
               
-              <li>
-                <button type="button" class="midbutton">HP</button>
-              </li>
 
-            </ul>
-        </div>
-      
-    </div>
-    
+              <ul>
+                <li>
+                  <button type="button" class="midbutton"><img class="drop" src="<?php echo imgfile;?>/drop.png" alt="">WET LAB</button>
+                </li>
+                
+                <li>
+                  <button type="button" class="midbutton"><img class="drop" src="<?php echo imgfile;?>/drop.png" alt="">DRY LAB</button>
+                </li>
+                
+                <li>
+                  <button type="button" class="midbutton"><img class="drop" src="<?php echo imgfile;?>/drop.png" alt="">HUMAN PRACTICE</button>
+                </li>
+              </ul>
+
+
+
+
+          </div>
+
+      </div>
+
+
+
+
+
+
   </div>
 
-  </div> 
-
-
-  
 
   <br>
   <br>
@@ -131,17 +116,7 @@
   <br>
   <br>
   <br>
-
-
-
-
 
   <?php include "./sub/footer.php"?>
-  
 
-     
-    
-        
-      
-    
 </body>
