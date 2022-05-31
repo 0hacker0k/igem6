781318c0207ceb6b1b9f4028626bc18b414b0490
SrrrrrrRb <cheryl20010902@gmail.com>
2022-05-31 23:20:12 +0800

    font changed

---
+++ notebook_dl_5.php
@@ -0,0 +1,105 @@
+<head>
+  
+  <title>wiki</title>
+  <?php include "./sub/config.php";?>
+  <?php include "./sub/head.php";?>
+  <link rel="stylesheet" media="all" href="<?php echo cssfile;?>/notebook_dl_5.css">
+  
+</head>
+
+<body>
+
+  
+  <?php include "./sub/header.php"?>
+
+  
+
+  <div class="blank">
+
+      <div class="top">
+
+          <ul>
+            <li>
+                <button type="button" class="toplabel toplabel1">WET LAB</button>
+            </li>
+
+            <li>
+                <button type="button" class="toplabel">DRY LAB</button>
+            </li>
+
+            <li>
+                <button type="button" class="toplabel">HP</button>
+            </li>
+
+            <li>
+                <button type="button" class="toplabel">TIMELINE</button>
+            </li>
+          </ul>
+
+      </div>
+
+      <div class="left">
+          
+        <ul>
+            <li>
+                <button type="button" class="leftlabel leftlabel1">MAY.</button>
+            </li>
+
+            <li>
+                <button type="button" class="leftlabel leftlabel2">JUN.</button>
+            </li>
+
+            <li>
+                <button type="button" class="leftlabel leftlabel3">JUL.</button>
+            </li>
+
+            <li>
+                <button type="button" class="leftlabel leftlabel4">AUG.</button>
+            </li>
+
+            <li>
+                <button type="button" class="leftlabel leftlabel5">SEP.</button>
+            </li>
+
+            <li>
+                <button type="button" class="leftlabel leftlabel6">OCT.</button>
+            </li>
+        </ul>
+
+      </div>
+
+      <div class="out">
+
+        <h1>May</h1>
+        
+        <div class="leftt"></div>
+
+        <!-- <img class="notebookmay" src="<?php echo imgfile;?>/notebook/5_1.png" alt=""> -->
+          
+        <div class="rightt"></div>
+              
+              
+
+          
+
+
+
+
+          
+      </div>
+
+
+
+
+
+
+  </div>
+
+  <div class="blankkk"></div>
+
+
+  
+
+  <?php include "./sub/footer.php"?>
+
+</body>
