f40ac4a4d626aa3a1e3706cb365897283f53ae2a
0hacker0k <55192104+0hacker0k@users.noreply.github.com>
2022-06-08 18:21:39 +0800

    iot debug

--- iot/iot.php
+++ iot/iot.php
@@ -3,9 +3,9 @@ include_once "./DBconnect.php";
 $DB = new DBconnect();
 $dbc = $DB->connect();
 $rs=$dbc->prepare("SELECT * FROM `iot`");
-$all=$rs->execute()->fetchall();
+$rs->execute();
 $count=0;
-foreach($all as $value){
+foreach($rs->fetchall() as $value){
     if($count!=0) echo " ";
     echo $value['status'];
     $count++;
