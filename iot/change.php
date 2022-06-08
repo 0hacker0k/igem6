e54b3c1d5558c482f3faf9b00fa7e79191c0b5cd
0hacker0k <55192104+0hacker0k@users.noreply.github.com>
2022-06-08 21:07:29 +0800

    iot

--- iot/change.php
+++ iot/change.php
@@ -1,4 +1,4 @@
 <form enctype="multipart/form-data" id="event_upload_form" method="post">
-    <button formaction="./lib.php?data=1" type="submit">1</button>
-    <button formaction="./lib.php?data=0" type="submit">0</button>
+    <button style="width: 30%;height:30%;font-size: 8rem;" formaction="./lib.php?data=1" type="submit">1</button>
+    <button style="width: 30%;height:30%;font-size: 8rem;" formaction="./lib.php?data=0" type="submit">0</button>
 </form>
