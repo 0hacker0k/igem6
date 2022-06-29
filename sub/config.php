32351d1c7f0702b6b2949bb72a724896c123e713
0hacker0k <55192104+0hacker0k@users.noreply.github.com>
2022-06-29 18:23:59 +0800

    change mod

--- sub/config.php
+++ sub/config.php
@@ -2,7 +2,7 @@
 // define("root",substr(dirname(__FILE__,2),strlen($_SERVER['DOCUMENT_ROOT'])));
 // echo dirname(__FILE__,2);
 // echo dirname($_SERVER['DOCUMENT_ROOT']);
-$htmlmode=1;
+$htmlmode=0;
 if($htmlmode==1 || dirname(__FILE__,2)=="/data1/adm/webteam.ccu.edu.tw/WWW/phaser"){
     define("root","/phaser");
 }else{
