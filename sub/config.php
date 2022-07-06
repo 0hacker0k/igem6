84e56d115189a3c28d445625c8f90d7d7f83cb20
0hacker0k <55192104+0hacker0k@users.noreply.github.com>
2022-07-06 12:41:55 +0800

    debug

--- sub/config.php
+++ sub/config.php
@@ -6,7 +6,7 @@ $htmlmode=1;//0:強制以特定名字 1:自適應 2:最終輸出模式(暫定)
 if($htmlmode==1 && dirname(__FILE__,2)=="/data1/adm/webteam.ccu.edu.tw/WWW/phaser"){
     define("root","/phaser");
 }else if($htmlmode==1 && dirname(__FILE__,2)=="/.CSDATA_NFS/home/under/u108/lyang108u/WWW/igem6"){
-    define("root","/igem6");
+    define("root","/~lyang108u/igem6");
 }else if($htmlmode==2){
     define("root","/igem6_final");
 }else if($htmlmode==0){
