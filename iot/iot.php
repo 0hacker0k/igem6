a64ed94fac6dac077e3da633647aaac51ec2db04
0hacker0k <55192104+0hacker0k@users.noreply.github.com>
2022-07-09 15:59:02 +0800

    iot modify

--- iot/iot.php
+++ iot/iot.php
@@ -6,10 +6,11 @@ $rs=$dbc->prepare("SELECT * FROM `iot`");
 $rs->execute();
 $count=0;
 foreach($rs->fetchall() as $value){
-    echo $value['status']." ";
-    echo $value['buzzer']." ";
-    echo $value['motor']." ";
-    echo $value['set_time']." ";
-    echo $value['return_value']." ";
+    echo $value['status']."\n";
+    echo $value['buzzer']."\n";
+    echo $value['motor']."\n";
+    echo $value['set_time']."\n";
+    echo $value['return_value']."\n";
+    echo $value['port']."\n";
     $count++;
 }
