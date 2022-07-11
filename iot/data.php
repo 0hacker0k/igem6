9d9ca4fee5e21ea087e6dd0061d84abf3404ffb8
0hacker0k <55192104+0hacker0k@users.noreply.github.com>
2022-07-11 14:54:30 +0800

    iot

--- iot/data.php
+++ iot/data.php
@@ -5,16 +5,6 @@ $dbc = $DB->connect();
 $rs=$dbc->prepare("SELECT * FROM `iot`");
 $rs->execute();
 $count=0;
-foreach($rs->fetchall() as $value){
-    echo $value['status']."\n";//狀態 0待命 1運作中 2異常
-    echo $value['count']."\n";//目前膠數
-    echo $value['command']."\n";//命令 0暫停 1開始 2清除後續工作 3全部停止
-    echo $value['concentration']."\n";//濃度
-    echo $value['set_time']."\n";//定時結束
-    echo $value['return_value']."\n";//已接收
-    echo $value['port']."\n";//運作ip
-    $count++;
-}
 if(isset($_GET['port'])){
     $rs=$dbc->prepare("UPDATE `iot` SET `port`=:dat WHERE `id`=1");
     $rs->bindValue("dat",$_GET['port']);
@@ -29,6 +19,16 @@ if(isset($_GET['count'])){
     $rs=$dbc->prepare("UPDATE `iot` SET `count`=:dat WHERE `id`=1");
     $rs->bindValue("dat",$_GET['count']);
     $v=$rs->execute();
+}
+foreach($rs->fetchall() as $value){
+    echo $value['status']."\n";//狀態 0待命 1運作中 2異常
+    echo $value['count']."\n";//目前膠數
+    echo $value['command']."\n";//命令 0暫停 1開始 2清除後續工作 3全部停止
+    echo $value['concentration']."\n";//濃度
+    echo $value['set_time']."\n";//定時結束
+    echo $value['return_value']."\n";//已接收
+    echo $value['port']."\n";//運作ip
+    $count++;
 }
 // if(isset($_GET['command'])){
 //     $rs=$dbc->prepare("UPDATE `iot` SET `command`=:dat WHERE `id`=1");
