8dc05744ea2611e51386d9e9fd359ebe1549da28
0hacker0k <55192104+0hacker0k@users.noreply.github.com>
2022-07-11 15:19:48 +0800

    iot

--- iot/data.php
+++ iot/data.php
@@ -15,8 +15,8 @@ foreach($rs->fetchall() as $value){
     echo $value['count']."\n";//目前膠數
     echo $value['command']."\n";//命令 0暫停 1開始 2清除後續工作 3全部停止
     echo $value['concentration']."\n";//濃度
-    echo $value['set_time']."\n";//定時結束
     echo $value['return_value']."\n";//已接收
+    echo $value['set_time']."\n";//定時結束
     echo $value['port']."\n";//運作ip
     $count++;
 }
