7d4d5f41f588a1fcbb603daa96570004e15f6f73
0hacker0k <55192104+0hacker0k@users.noreply.github.com>
2022-07-11 17:45:30 +0800

    iot

--- iot/data.php
+++ iot/data.php
@@ -2,14 +2,14 @@
 include_once "./DBconnect.php";
 $DB = new DBconnect();
 $dbc = $DB->connect();
-$rs=$dbc->prepare("SELECT * FROM `iot`");
-$rs->execute();
-$count=0;
 if(isset($_GET['status'])){
     $rs=$dbc->prepare("UPDATE `iot` SET `status`=:dat WHERE `id`=1");
     $rs->bindValue("dat",$_GET['status']);
     $v=$rs->execute();
 }
+$rs=$dbc->prepare("SELECT * FROM `iot`");
+$rs->execute();
+$count=0;
 foreach($rs->fetchall() as $value){
     echo $value['status']."\n";//狀態 0待命 1運作中 2異常
     echo $value['count']."\n";//目前膠數
@@ -33,11 +33,11 @@ if(isset($_GET['port'])){
     $rs=$dbc->prepare("UPDATE `iot` SET `return_value`=1 WHERE `id`=1");
     $v=$rs->execute();
 }
-// if(isset($_GET['command'])){
-//     $rs=$dbc->prepare("UPDATE `iot` SET `command`=:dat WHERE `id`=1");
-//     $rs->bindValue("dat",$_GET['command']);
-//     $v=$rs->execute();
-// }
+if(isset($_GET['command'])){
+    $rs=$dbc->prepare("UPDATE `iot` SET `command`=:dat WHERE `id`=1");
+    $rs->bindValue("dat",$_GET['command']);
+    $v=$rs->execute();
+}
 
 
 // if(isset($_GET['set_time']) && $_GET['set_time']==1 && $_POST['set_time']!=null){
