670adbaf9b130ae4ee4e378b6117b43a5b8ba337
0hacker0k <55192104+0hacker0k@users.noreply.github.com>
2022-07-17 23:49:43 +0800

    attribution css

--- attribution.php
+++ attribution.php
@@ -9,30 +9,33 @@
     <?php include "./sub/header.php"?>
     <?php include "sub/loading.php"?>
     <img class="full_size_image" src="<?php echo imgfile;?>/human/attribution_banner.jpg" alt=""> 
-    <br>
-    <br>
-    <br>
-    <br>
-  
-
-    <div class="member_card row justify-content-around">
+    <div class="attribution_overview justify-content-around">
         <div class="col-7">
             <h1>Overview</h1>
             <p>CCU_Taiwan iGEM Team counts iGEM competition as a meaningful and challengeable project. During our iGEM journey, we want to give a massive thank you to everyone that giving us endless support, advice, and meanwhile professional teaching. Without those assists, we can't accomplish what we have gone through these days. We appreciate and are grateful for every single contribution given throughout our project.</p>
         </div>
-
-        <div class="col-4 sticker" style="background-image:url(<?php echo imgfile;?>/note_untouch.png")>
-            <ul>
-                <button>Primary Principal Investigator</button>
-                <button>Secondary Principal Investigators</button>
-                <button>Instuctor</button>
-                <button>Leaders</button>
-                <button>wet lab</button>
-                <button>dry lab</button>
-                <button>human practice</button>
-            </ul>
+    </div>
+    <div class="attribution_card justify-content-around">
+        <div class="attribution_left">
+            <div class="attribution_nav" style="background-image:url(<?php echo imgfile;?>/note_untouch.png")>
+                <div class="attribution_nav_content">
+                    <div class="nav_control"><li>Primary Principal Investigator</li></div>
+                    <div class="nav_control"><li>Secondary Principal Investigators</li></div>
+                    <div class="nav_control"><li>Instuctor</li></div>
+                    <div class="nav_control"><li>Leaders</li></div>
+                    <div class="nav_control"><li>wet lab</li></div>
+                    <div class="nav_control"><li>dry lab</li></div>
+                    <div class="nav_control"><li>human practice</li></div>
+                </div>    
+            </div>
         </div>
-
-        <h1 style="background-image:url(<?php echo imgfile;?>/tape.png")>Leader</h1>
+        <div class="attribution_right">
+            <div class="title-label-div">
+                <h1 class="title-label" style="background-image:url(<?php echo imgfile;?>/tape.png")>Leader</h1>
+            </div>
+        </div>
+        
+        
+    </div>
     <?php include "./sub/footer.php"?>   
 </body>
