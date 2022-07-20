e772a420e12f949f615bad3f21a62ad280195559
0hacker0k <55192104+0hacker0k@users.noreply.github.com>
2022-07-20 15:33:19 +0800

    loading page

---
+++ loading.php
@@ -0,0 +1,16 @@
+<!doctype html> 
+<head>
+  
+  <title>wiki</title>
+  <meta charset="UTF-8" />
+  <?php include "./sub/config.php";?>
+  <?php include "./sub/head.php";?>
+  <link rel="stylesheet" media="all" href="<?php echo cssfile;?>/notebook.css">
+  <script src="<?php echo jsfile;?>/notebook.js"></script>
+  
+</head>
+
+<body onload="">
+  <?php include "./sub/header.php"?>
+  <?php include "sub/loading.php"?>
+</body>
