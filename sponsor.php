de22e24efd3d10cd23096e396175ed918f486c27
0hacker0k <55192104+0hacker0k@users.noreply.github.com>
2022-08-06 22:23:06 +0800

    sponsor card

--- sponsor.php
+++ sponsor.php
@@ -10,9 +10,20 @@
     <?php include "sub/loading.php"?>
     <img class="full_size_image" src="<?php echo imgfile;?>/sponsor/pepe-the-frog-gif-6.gif" alt=""> 
     <div class="sponsor_body">
-        <img class="sponsor_rope" src="<?php echo imgfile;?>/sponsor/rope.png" alt="">
-        <img class="sponsor_clip" src="<?php echo imgfile;?>/sponsor/clip_white.png" alt="">
-        <img class="sponsor_paper" src="<?php echo imgfile;?>/sponsor/paper_white.png" alt="">
+        <img class="sponsor_rope" style="--i:0" src="<?php echo imgfile;?>/sponsor/rope.png" alt="">
+        <img class="sponsor_rope" style="--i:1" src="<?php echo imgfile;?>/sponsor/rope.png" alt="">
+        <div class="sponsor_card" style="--i:0">
+            <img class="sponsor_clip" src="<?php echo imgfile;?>/sponsor/clip_white.png" alt="">
+            <img class="sponsor_paper" src="<?php echo imgfile;?>/sponsor/paper_white.png" alt="">
+        </div>
+        <div class="sponsor_card" style="--i:1">
+            <img class="sponsor_clip" src="<?php echo imgfile;?>/sponsor/clip_white.png" alt="">
+            <img class="sponsor_paper" src="<?php echo imgfile;?>/sponsor/paper_white.png" alt="">
+        </div>
+        <div class="sponsor_card" style="--i:2">
+            <img class="sponsor_clip" src="<?php echo imgfile;?>/sponsor/clip_white.png" alt="">
+            <img class="sponsor_paper" src="<?php echo imgfile;?>/sponsor/paper_white.png" alt="">
+        </div>
     </div>
 
 
