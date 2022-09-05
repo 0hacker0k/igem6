b324dc940c73c2266f91ab899976e762d5a686a7
SrrrrrrRb <cheryl20010902@gmail.com>
2022-09-05 18:51:49 +0800

    hp notebook july

---
+++ notebook/notebook_hp_7.php
@@ -0,0 +1,102 @@
+<?php include "../sub/config.php";?>
+            <div class="pad">
+                <div class="week">
+                    <img class="week_content" src="<?php echo imgfile;?>/notebook/hp_6.jpg" alt="">
+                    <div class="week_content">
+                        
+                        <h5 class="title_content">Week 10 (7/3-9)</h5>
+                        
+                        <div class="word_content">
+                        <ul>
+                            <li>Planning courses for the Open Lab.</li>
+                            <br>
+                            <li>Designing the online cookbook of collaboration.</li>
+                            <br>
+                            <li>Illustrating the scenes of the promotion video.</li>
+                            <br>
+                            <li>Posting member introductions on social media.</li>                         
+                        </ul>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="week">
+                    
+                    <div class="week_content">
+                        
+                        <h5 class="title_content">Week 11 (7/10-16)</h5>
+                        
+                        <div class="word_content">
+                        <ul>
+                            <li>Completing illustrations of stage 4 of the web game and revising the story outline.</li>
+                            <br>
+                            <li>Completing the design of flyers for Campus walking.</li>
+                            <br>
+                            <li>Planning the agenda and assigning tasks of the Open Lab.</li>
+                            <br>
+                            <li>Designing the interview abstract (toward dietitians).</li>
+                            <br>
+                            <li>Filming the promotion video.</li>
+                            <br>
+                            <li>Searching local businesses for fundraising.</li>
+                            <br>
+                            <li>Sending sponsorship emails to enterprises.</li>
+                        </ul>
+                        </div>
+                    </div>
+                    <img class="week_content" src="<?php echo imgfile;?>/notebook/hp_7.jpg" alt="">
+                </div>
+
+                <div class="week">
+                    
+                    <img class="week_content" src="<?php echo imgfile;?>/notebook/hp_8.jpg" alt="">
+                    <div class="week_content">
+                        
+                        <h5 class="title_content">Week 12 (7/17-23)</h5>
+                        
+                        <div class="word_content">
+                        <ul>
+                            <li>Designing stage 5 of the web game and completing dialog boxes.</li>
+                            <br>
+                            <li>Amending the venue of campus walking.</li>
+                            <br>
+                            <li>Sending collaboration emails to other iGEM teams.</li>
+                            <br>
+                            <li>Refining the interview abstract (toward dietitians).</li>
+                            <br>
+                            <li>Designing the team business card and the poster for fundraising.</li>
+                            <br>
+                            <li>Promoting the Open Lab on our social media.</li>                     
+                        </ul>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="week">
+                    
+                    <div class="week_content">
+                        
+                        <h5 class="title_content">Week 13 (7/24-30)</h5>
+                        
+                        <div class="word_content">
+                        <ul>
+                            <li>Completing illustrations of stage 5 of the web game.</li>
+                            <br>
+                            <li>Visiting local villages to promote our campus walking.</li>
+                            <br>
+                            <li>Composing teaching materials for the Open Lab.</li>
+                            <br>
+                            <li>Compiling the rundown for the TCUS entrepreneurship competition.</li>
+                            <br>                            
+                            <li>Interviewing the pharmacist, Chi-Kai Wu.</li>
+                            <br>
+                            <li>Sending sponsorship emails to enterprises.</li>
+                        </ul>
+                        </div>
+                    </div>
+
+   
+                    <img class="week_content" src="<?php echo imgfile;?>/notebook/hp_9.png" alt="">
+                </div>
+
+            </div>
