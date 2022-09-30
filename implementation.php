72ae0508a3b74a928ca6ae0488d4fefe7da3a1a6
SrrrrrrRb <cheryl20010902@gmail.com>
2022-10-01 01:39:54 +0800

    create implementation

---
+++ implementation.php
@@ -0,0 +1,149 @@
+
+<head>
+    <title>wiki</title>
+    <?php include "./sub/config.php";?>
+    <?php include "./sub/head.php";?>
+    <link rel="stylesheet" media="all" href="<?php echo cssfile;?>/common.css">
+    
+</head>
+
+<body onload="load_finish();">
+    <?php include "./sub/header.php"?>
+    <?php include "sub/loading.php"?>
+    
+    <div class="overview justify-content-around">
+        <div class="col-7">
+            <div id="1" class="title-label-div">
+                <h1 class="title_label" style="background-image:url('<?php echo imgfile;?>/tape.png')">Implementation</h1>
+            </div>
+            <p>
+                Being the most notorious, life-taking disease in this era, the Cardiovascular disease ( CVD ) issue is definitely urged to be resolved. 
+            </p>
+            <p>
+                To prevent CVD, we hope to use synthetic biology to produce pure Eicosapentaenoic acid ( EPA ) supplements serving as the uncontaminated, effective alternative. Applying this idea to reality, we had been challenged by many other experts. With their professional suggestions, we can shift the naive idea into a comprehensive resolution that can address the problem properly. In light of the marketing idea, we also plan to create an enhanced version of our product form to suit the market more.
+            </p>
+        </div>
+    </div>
+
+    <br>
+    <br>
+
+    <div class="article">
+        <div class="in_center">
+            <div id="2" class="title-label-div">
+                <h1 class="title_label" style="background-image:url('<?php echo imgfile;?>/tape.png')">Clinical Studies</h1>
+            </div>
+            <p>
+                &emsp;&emsp;Let's start with the mechanism of how EPA reduces the chance of CVD. The major cause of CVD is Atherosclerosis, which is caused by atheromas. When the endothelium cells of blood vessels get injured, fatty acids like low - density lipid ( LDL ) and cholesterol accumulates and forms atheromatous plaque, which leads to vascular sclerosis and causes the CVD further. EPA can reduce such a situation. According to the reference, EPA can induce the enzymes  producing the substances which can relax the smooth muscle cell and provide anti – inflammatory action, leading to the reduction of LDL, so the occurrence of CVD will decrease.
+            </p>
+            <br>
+            <div id="3" class="title-label-div">
+                <h1 class="title_label" style="background-image:url('<?php echo imgfile;?>/tape.png')">Target User</h1>
+            </div>
+            <p>
+                &emsp;&emsp;According to the statistical data of WHO, in 2021, there were 32% of the deaths caused by CVD in the world. In the meanwhile, in Taiwan, there were more than 42 thousand people dead because of CVD, only next to cancer. So the problem of CVD cannot be underestimated. The common symptom of CVD include: 
+            </p>
+            <ul>
+                <li class="biggerli">
+                    Chest pain, chest tightness, chest pressure and chest discomfort (angina)
+                </li>
+                <li class="biggerli">
+                    Shortness of breath
+                </li>
+                <li class="biggerli">
+                    Pain in the neck, jaw, throat, upper belly area or back
+                </li>
+                <li class="biggerli">
+                    Pain, numbness, weakness or coldness in the legs or arms if the blood vessels in those body areas are narrowed
+                </li>
+            </ul>
+            <p>
+                &emsp;&emsp;When the symptoms above happen, the only way to solve it is seeking emergency healthcare. Luckily, we can do something to prevent us from getting CVD. Generally speaking, the elders are the main group who will get CVD easily. So, we not only held an activity to promote the concepts of CVD prevention, like doing sports more often, consuming less fried food, but also regarded them as our target users. But, CVD is a kind of degenerative disease, which means even if you are young, you still have the chance of getting CVD. As a result, during Open lab activity, we also emphasized the importance of CVD.
+            </p>
+            <br>
+            <div id="4" class="title-label-div">
+                <h1 class="title_label" style="background-image:url('<?php echo imgfile;?>/tape.png')">Product Design</h1>
+            </div>
+            <p>
+                &emsp;&emsp;The bottle is made of deep colored plastic. Although the plastic can isolate major visible lights, we want to reduce the influence of lights to minimize. According to an expert's suggestion, the consumption for a person of EPA a day is 1 gram, and that will be contained in the capsule. Take the convenience into consideration, the capacity of the capsule for children under 12 year old is halving. The capsule shell is made of carrageenans in order to meet the needs of vegetarians. If there is a person who wants to try our product, they probably try it for a month. So, one bottle will contain thirty capsules. Otherwise, we will pack two or four bottles in one box for the people who are too lazy to buy the products frequently.
+            </p>
+            <br>
+            <div id="5" class="title-label-div">
+                <h1 class="title_label" style="background-image:url('<?php echo imgfile;?>/tape.png')">Safety</h1>
+            </div>
+            <br>
+            <div id="6" class="title-label-div">
+                <h1 class="title_label" style="background-image:url('<?php echo imgfile;?>/tape.png')">Advantage</h1>
+            </div>
+            <ul>
+                <li>
+                    <h2 class="biggerli">High purity:</h2>
+                    <p>
+                        According to reference, pfa B in the pfa gene can decide the kind of omega – 3 bacteria produced. Compared with the fish oil, our PACOmega can more effectively focus on CVD prevention.
+                    </p>
+                </li>
+                <li>
+                    <h2 class="biggerli">Zero contamination of heavy metal and microplastics:</h2>
+                    <p>
+                        Contrast with EPA extracted from fish oil, our PACOmega is extracted from E. coli, which can solve the problem of the contamination of heavy metal and microplastics.
+                    </p>
+                </li>
+                <li>
+                    <h2 class="biggerli">Vegetarian friendly</h2>
+                    <p>
+                        Major EPA source is fish oil, which is inconvenient for vegetarians. Therefore, we provide PACOmega, another source of EPA, when vegetarians reject the EPA from fish oil.
+                    </p>
+                </li>
+            </ul>
+            <div id="6" class="title-label-div">
+                <h1 class="title_label" style="background-image:url('<?php echo imgfile;?>/tape.png')">Challenge</h1>
+            </div>
+            <p>
+                &emsp;&emsp;According to the reference, we found that bacteria can produce pure EPA. However, the information of the actual production can be hardly found. We considered that the actual production is not big enough to be commercial. So, after making sure the EPA system we design works, we will transform it into the bacteria which is suitable for such work.
+            </p>
+            <div id="6" class="title-label-div">
+                <h1 class="title_label" style="background-image:url('<?php echo imgfile;?>/tape.png')">Future Work</h1>
+            </div>
+            <p>
+                <ol>
+                    <li class=biggerli>
+                        Keeping on finishing our design, making sure the E. coli can produce EPA and improving production.
+                    </li>
+                    <li class=biggerli>
+                        Transforming our EPA synthy system into the bacteria which is suitable for abundant production.
+                    </li>
+                </ol>
+            </p> 
+            <br>
+            <br>    
+            <p class="h25">
+                Reference:
+                <ol>
+                    <li>
+                        Davis, M. S., J. Solbiati and J. E. Cronan, Jr. (2000). "Overproduction of acetyl-CoA carboxylase activity increases the rate of fatty acid biosynthesis in Escherichia coli." J Biol Chem 275(37): 28593-28598.
+                    </li>
+                    <li>
+                        Okuyama, H., Y. Orikasa, T. Nishida, K. Watanabe and N. Morita (2007). "Bacterial genes responsible for the biosynthesis of eicosapentaenoic and docosahexaenoic acids and their heterologous expression." Appl Environ Microbiol 73(3): 665-670.
+                    </li>
+                    <li>
+                        Orikasa, Y., T. Nishida, A. Yamada, R. Yu, K. Watanabe, A. Hase, N. Morita and H. Okuyama (2006). "Recombinant production of docosahexaenoic acid in a polyketide biosynthesis mode in Escherichia coli." Biotechnol Lett 28(22): 1841-1847.
+                    </li>
+                        Orikasa, Y., M. Tanaka, S. Sugihara, R. Hori, T. Nishida, A. Ueno, N. Morita, Y. Yano, K. Yamamoto, A. Shibahara, H. Hayashi, Y. Yamada, A. Yamada, R. Yu, K. Watanabe and H. Okuyama (2009). "pfaB products determine the molecular species produced in bacterial polyunsaturated fatty acid biosynthesis." FEMS Microbiol Lett 295(2): 170-176.
+                    <li>
+                        Orikasa, Y., A. Yamada, R. Yu, Y. Ito, T. Nishida, I. Yumoto, K. Watanabe and H. Okuyama (2004). "Characterization of the eicosapentaenoic acid biosynthesis gene cluster from Shewanella sp. strain SCRC-2738." Cell Mol Biol (Noisy-le-grand) 50(5): 625-630.
+                    </li>
+                    <li>
+                        Rosano, G. L. and E. A. Ceccarelli (2014). "Recombinant protein expression in Escherichia coli: advances and challenges." Front Microbiol 5: 172.
+                    </li>
+                    <br>
+                    <br>
+                </ol>
+            </p>
+        </div>
+    </div>
+    
+
+
+    <?php include "./sub/footer.php"?>
+    <script src="<?php echo jsfile;?>/common.js"></script>  
+</body>
