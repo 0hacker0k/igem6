29ca4536d48d3ca9ed6bb3cd39e7c3647e7cda89
SrrrrrrRb <cheryl20010902@gmail.com>
2022-10-01 12:12:28 +0800

    correct collaboration

--- collaborations.php
+++ collaborations.php
@@ -25,7 +25,7 @@
                 <h1 class="title_label" style="background-image:url('<?php echo imgfile;?>/tape.png')">COLLABORATION</h1>
             </div>
             <p>
-                &emsp;&emsp;&emsp;During our IHP interview with the dietitian Miranda Chen ( link of dietitian’s IHP page ), she told us that EPA supplement alone wasn’t sufficient enough to prevent CVD. From her professional perspective, she suggested that we should also value the importance of having a healthy diet during daily life.
+                &emsp;&emsp;&emsp;During our IHP interview with the dietitian Miranda Chen, she told us that EPA supplement alone wasn’t sufficient enough to prevent CVD. From her professional perspective, she suggested that we should also value the importance of having a healthy diet during daily life.
             </p>
             <p>
                 &emsp;&emsp;&emsp;To promote healthy diets to every citizen on this beautiful planet, we initiated a CVD-Free Cookbook, inviting iGEM teams from all around the world to share special ingredients in their countries that are good for cardiovascular health. Therefore, our cookbook features cuisines that are exotic but also healthy so that people who intend to care for their vessels can check it out for ideas of daily diets.
@@ -34,7 +34,7 @@
                 &emsp;&emsp;&emsp;The following are our CookBook partner teams: Mingdao, NYCU_Formosa, NCKU, Gems_Taiwan, Korea_HS, Copenhagen, iGEM_CSMU, and CU_Egypt.
             </p>
             <p>
-                &emsp;&emsp;&emsp;During our IHP interview with the dietitian Miranda Chen ( link of dietitian's IHP page ), she told us that EPA supplement alone wasn't sufficient enough to prevent CVD. From her professional perspective, she suggested that we should also value the importance of having a healthy diet during daily life.
+                &emsp;&emsp;&emsp;During our IHP interview with the dietitian Miranda Chen, she told us that EPA supplement alone wasn't sufficient enough to prevent CVD. From her professional perspective, she suggested that we should also value the importance of having a healthy diet during daily life.
                 To promote healthy diets to every citizen on this beautiful planet, we initiated a CVD-Free Cookbook, inviting iGEM teams from all around the world to share special ingredients in their countries that are good for cardiovascular health. Therefore, our cookbook features cuisines that are exotic but also healthy so that people who intend to care for their vessels can check it out for ideas of daily diets.
                 The following are our CookBook partner teams: Mingdao, NYCU_Formosa, NCKU, Gems_Taiwan, Korea_HS, Copenhagen, iGEM_CSMU, and CU_Egypt.
             </p>
@@ -71,7 +71,7 @@
                 <h2 class="title_sub_label" style="background-image: url('/igem6/img/highlight.png')">Korea_HS</h1>
             </div>
             <p>
-                &emsp;&emsp;&emsp;Our virtual meeting with iGEM Korea_HS exchanged both of our projects’ ideas. We gave comments on each other’s presentations and asked questions about both of our improvements, including details about how we integrate into society, and how to be innovative in promoting each of our projects. After the meeting, they asked us to participate in several of their projects and conferences they held (link of Korea_HS’s badge project), and we surely gained a lot of new abilities and knowledge due to these collaboration opportunities.
+                &emsp;&emsp;&emsp;Our virtual meeting with iGEM Korea_HS exchanged both of our projects’ ideas. We gave comments on each other’s presentations and asked questions about both of our improvements, including details about how we integrate into society, and how to be innovative in promoting each of our projects. After the meeting, they asked us to participate in several of their projects and conferences they held, and we surely gained a lot of new abilities and knowledge due to these collaboration opportunities.
             </p>
             <div id="4" class="title-label-div">
                 <h2 class="title_sub_label" style="background-image: url('/igem6/img/highlight.png')">Copenhagan</h1>
@@ -87,7 +87,7 @@
                 &emsp;&emsp;&emsp;On 8/22, we joined Taiwan iGEM meet ‘22 to exchange our ideas with other Taiwan iGEM teams. This meet-up was hosted by NCKU_Tainan iGEM team, and the participants include NCHU_Taichung, iGEM NTHU, NYCU_Formosa, iGEM KCIS Xiugang, iGEM CSMU, iGEM Mingdao, GEMS Taiwan, NYCU_Taipei, and our team. During the conference, all participants gathered together to discuss each team’s projects and gave concrete feedback in the hope of improving, including presentation and inspecting and learning each other’s poster.
             </p>
             <p>
-                &emsp;&emsp;&emsp;After the conference, we were inspired to further examine our Quantitative Agar Faucet (link of DL’s quantitative agar faucet). As a result, we reached out to Prof. Lee (link of professor Lee’s IHP page) to explore potential development opportunities. Besides, this meetup gave us a chance to have deeper communication with teams around Taiwan, and is a leading of many of the following collaborations, including the the iGEM theme song project with CSMU_Taiwan (link), iGEM map with NTHU_Taiwan (link), and the infographic exchange with Gems_Taiwan (link).
+                &emsp;&emsp;&emsp;After the conference, we were inspired to further examine our Quantitative Agar Faucet . As a result, we reached out to Prof. Lee to explore potential development opportunities. Besides, this meetup gave us a chance to have deeper communication with teams around Taiwan, and is a leading of many of the following collaborations, including the the iGEM theme song project with CSMU_Taiwan, iGEM map with NTHU_Taiwan, and the infographic exchange with Gems_Taiwan.
             </p>
             <div class="full_block">
                 <div class="img" style="--width:30%;">
