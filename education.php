b3c9c9214b60a7cfb7694bedc7de1114758350df
SrrrrrrRb <cheryl20010902@gmail.com>
2022-10-01 10:54:25 +0800

    create education

---
+++ education.php
@@ -0,0 +1,147 @@
+
+<head>
+    <title>wiki</title>
+    <?php include "./sub/config.php";?>
+    <?php include "./sub/head.php";?>
+    <link rel="stylesheet" media="all" href="<?php echo cssfile;?>/common.css">
+    
+</head>
+
+<body onload="load_finish();">
+    <?php include "./sub/header.php"?>
+    <?php include "sub/loading.php"?>
+    
+    <div class="overview justify-content-around">
+        <div class="col-7">
+            <div id="1" class="title-label-div">
+                <h1 class="title_label" style="background-image:url('<?php echo imgfile;?>/tape.png')">Education</h1>
+            </div>
+        </div>
+    </div>
+    
+    <div class="article">
+        <div class="in_center">
+            <h1>Overview</h1>
+            <p>
+                &emsp;&emsp;Aside from the lab work, science communication plays as an essential role as the experimentation does in application. With proper interaction and brain-storming activities, we as responsible and reliable scientists, can truly share the same level of standpoints with our target audience and explore mutual communication. Therefore, we attempt to share information about synthetic biology and our project to relative communities.
+            </p>
+            <p>
+                &emsp;&emsp;Our education and science communication projects mainly focus on the two goals: a. introducing synthetic biology to the public and b. raise the awareness of CVD prevention. These two are tethered together because lacking any of it might end up failing to tell the whole story of our project: utilizing the synthetic biological way to create pure EPA supplements for arteries care. We have observed that most of the people we contacted have an ambiguous and one-sided understanding of these two fields. As a result, they have difficulty connecting them in a comprehensive way. We solve the problem by constructing a coherent story from the current problem to our resolution.
+            </p>
+            <p>
+                &emsp;&emsp;The story is as follows: To prevent CVD, we want to use EPA which has been proven to have a good effect on reducing cardiac events. Our approaches to CVD prevention include producing pure EPA by synthetic biology and promoting a healthy lifestyle.
+            </p>
+            <p>
+                &emsp;&emsp;Having this in mind, we held an Open Lab activity for high school students intended to share the synthetic biological experiments and initiated a campus walking activity to draw more attention to CVD prevention. In echoing the World Heart Day activity in Chiayi, we also gathered together on September 29th to send more information about the arterial care.
+            </p>
+            <p>
+                &emsp;&emsp;To make our idea more accessible, we designed a web game to promote CVD prevention virtually and remotely. Combining the idea of synthetic biology and CVD prevention, we created a storyline in which the main character, PACO, is dispatched to an adventure that’s wrapped in the theme of experimentation to save her kingdom from illness.
+            </p>
+            <p>
+                &emsp;&emsp;With those attempts, we sincerely hope that we can really make efforts to bridge the gap between society and the academic field. And embracing multilateral collaboration.
+            </p>
+            <div id="2" class="title-label-div">
+                <h1 class="title_label" style="background-image:url('<?php echo imgfile;?>/tape.png')">Open Lab</h1>
+            </div>
+            <p>
+                &emsp;&emsp;In order to prevent CVD, we came up with a new way to synthesize EPA without contamination. However, we found that the idea of synthetic biology is too abstract for normal people to grab. As a result, to get people familiar with how we synthesize pure EPA, we held an Open Lab science camp to invite every high school student to get involved in synthetic biological experiments.
+            </p>
+            <div class="full_block">
+                <div class="img" style="--width:80%;">
+                    <img src="<?php echo imgfile;?>/education/education01.jpg">
+                    <div class="triangle_to_top"></div>Group photo - our team members with the participants
+                </div>
+            </div>
+            <h1>
+                How do we achieve the publicity of synthetic biology?
+            </h1>
+            <p class="h25">
+                &emsp;&emsp;To let participants precisely comprehend the production of our products, we decided to have participants conduct the experiments on hand. Meanwhile, with the explanation of the experimental principle and the importance of laboratory safety by our team members, we can provide participants with clear and comprehensive information on the technology we use.
+            </p>
+            <h1>Before the activity</h1>
+            <p class="h25">
+                &emsp;&emsp;Although we are well prepared with the course materials and experiment equipment that will be used in the course, it is uncertain for us to make sure whether the explanation and the teaching styles are suitable for people who have never met synthetic biology. As a result, we set rehearsals twice and invited peers in our campus with different majors to experience the course and make sure they can completely absorb the knowledge. They gave us some advice to improve our teaching styles. To illustrate, they find out that some of the principles and technical terms are too abstruse to comprehend, they suggest we use metaphors when expounding them. All these advices has perfectly helped us complete a excellent activity.
+            </p>
+            <div class="full_block">
+                <div class="img" style="--width:80%;">
+                    <img src="<?php echo imgfile;?>/education/education02.jpg">
+                    <div class="triangle_to_top"></div>First rehearsals - our teambers with campus peers
+                </div>
+            </div>
+            <h1>Record during the activity</h1>
+            <p class="h25">
+                &emsp;&emsp;In the experiment, to allow the operator to observe the results more effectively and understand the effect of the technology we adopt, we used green fluorescent protein (GFP), which is often used as a reporter gene in synthetic biology as the target gene and has been significantly used in the research field. Also, we use pET15b as plasmid and transform into E. coli. During the experiment, we lead them to start with gene selection, PCR, gel electrophoresis, purification, ligation, and transformation sequentially. Finally, observing the E. coli that expresses GFP protein.
+            </p>
+            <div class="full_block">
+                <div class="img" style="--width:80%;">
+                    <img src="<?php echo imgfile;?>/education/education03.jpg">
+                    <div class="triangle_to_top"></div>Letting participants precisely comprehend the production of our products
+                </div>
+            </div>
+            <div class="full_block">
+                <div class="img" style="--width:80%;">
+                    <img src="<?php echo imgfile;?>/education/education04.jpg">
+                    <div class="triangle_to_top"></div>Hard working on pipette using - team member and the participants
+                </div>
+            </div>
+            <div class="full_block">
+                <div class="img" style="--width:80%;">
+                    <img src="<?php echo imgfile;?>/education/education05.jpg">
+                    <div class="triangle_to_top"></div>Principle explanation - PCR mechanism 
+                </div>
+            </div>
+            <div class="full_block">
+                <div class="img" style="--width:80%;">
+                    <img src="<?php echo imgfile;?>/education/education06.jpg">
+                    <div class="triangle_to_top"></div>Experience of the participants - gel electrophoresis
+                </div>
+            </div>
+            <div class="full_block">
+                <div class="img" style="--width:80%;">
+                    <img src="<?php echo imgfile;?>/education/education07.jpg">
+                </div>
+            </div>
+            <h1>Feedback from participants</h1>
+            <p class="h25">
+                &emsp;&emsp;After the camp, we set up a quiz section through Kahoot, which is a platform that provides an interactive game to evaluate whether the participants have indeed obtained the knowledge. The data of results show that over 70% of them have well comprehended the principle we want to convey. Additionally, some participants also sent us a message after the activity, indicating that this activity efficiently piques their interest in synthetic biology, and also sets their misunderstanding with gene transfer straight.
+            </p>
+            <div class="full_block">
+                <div class="img" style="--width:40%;">
+                    <img src="<?php echo imgfile;?>/education/education08.jpg">
+                </div>
+                <div class="img" style="--width:40%;">
+                    <img src="<?php echo imgfile;?>/education/education09.jpg">
+                </div>
+            </div>
+            <div class="full_block">
+                <div class="triangle_to_top"></div>Kahoot section - first place (left) and top three (right) of the participants
+            </div>
+            <div id="3" class="title-label-div">
+                <h1 class="title_label" style="background-image:url('<?php echo imgfile;?>/tape.png')">Campus walking</h1>
+            </div>
+            <p>
+                &emsp;&emsp;According to the data from the Taiwanese government, the elderly percentage near our area is relatively higher than in other places in Taiwan, and through cross-comparison, cardiovascular disease causes great harm here. Hence, based on the research (Oxid Med Cell Longev, Apr 9, 2019, 3756750), we discover that aerobic exercise significantly affects lipid metabolism. Furthermore, we found that "walking" is the easiest way to achieve it in everyone’s daily life because we don’t have to use certain equipment and we can do it without time restrictions. As a result, we initiated a campus walk called ”Walking with the elderly, walking to a better heart.
+            </p>
+            <h1>Before the activity</h1>
+            <p class="h25">
+                &emsp;&emsp;Consequently, we cooperated with the “Center for Innovative Research on Aging Society (CIRAS)” in CCU, they provided us some advice and shared some experience on interacting with the elderly, for example, in the interacting game design, we amended some of the word usages that might mislead the public on CIRAS suggestion. In addition, we went to the village and the elderly university in CCU to invite them to our activity in person, hoping our sincere invitation can interest them to participate in the event. In the promotion phase, we know that some of the elderly seldom have an outdoor activity, therefore, this reinforces us to provide a great opportunity for them to walk with us. 
+            </p>
+            <h1>Record during the activity</h1>
+            <p class="h25">
+                &emsp;&emsp;On that day, almost a hundred people joined us in the event: “Walking with the elderly, walking to a better heart.” Through this event, we hope to raise public awareness through the interactive game, guiding them to know more about cardiovascular disease. Therefore, we divided the game into 5 stages throughout our walking route.  First, let the public know about the severity of cardiovascular disease. Second, knowing what type of living habit will lead to cardiovascular diseases? Third, how do we prevent cardiovascular disease in every day’s food consumption? Fourth, how do we prevent cardiovascular disease through different kinds of exercise? Last but not least, tested them if they got the ideas for the whole activity.
+            </p>
+            <h1>Feedback from participants</h1>
+            <p class="h25">
+                &emsp;&emsp;After the event, we received real-life experience in fighting cardiovascular disease from a participant, providing us with a stronger impetus on promoting this issue. A man around 55 years old told us that he suffered from cardiovascular disease 10 years ago, but since he started to do aerobic exercise regularly, the condition has become better and better, and currently, he is no longer having cardiovascular disease. Although the research tells us that aerobic exercise is helpful for cardiovascular disease, it is really touching to hear the real experience through this event. We hope that through our efforts, more people in the world can understand this issue and try to change their lifestyles.
+            </p>
+            <br>
+            <br>
+            <br>
+        </div>
+    </div>
+    
+
+
+    <?php include "./sub/footer.php"?>
+    <script src="<?php echo jsfile;?>/common.js"></script>  
+</body>
