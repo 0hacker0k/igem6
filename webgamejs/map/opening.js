570b3d2c4603bbe8a424f184ba9de617c020bbe0
tino455131 <55025249+tino455131@users.noreply.github.com>
2022-10-06 14:09:29 +0800

    webgame opening start button optimize

--- webgamejs/map/opening.js
+++ webgamejs/map/opening.js
@@ -24,6 +24,12 @@ function create_opening (){
             load_page(stage_0_story);
         },500);
     },this);
+    start.on("pointerover",function(){
+        start.setDisplaySize(width*0.14,height*0.14);
+    },this);
+    start.on("pointerout",function(){
+        start.setDisplaySize(width*0.12,height*0.12);
+    },this);
     //DEBUG
     if(debug==1){
         this.add.image(width/2, height/2, 'D_center').setOrigin(0.5, 0.5).setDisplaySize(10,10);
