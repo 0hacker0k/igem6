5ffbcb92592b884e53a66edddb891a9fd237ede3
SrrrrrrRb <cheryl20010902@gmail.com>
2022-10-09 16:45:53 +0800

    change body background img

--- sub/config.php
+++ sub/config.php
@@ -2,7 +2,7 @@
 // define("root",substr(dirname(__FILE__,2),strlen($_SERVER['DOCUMENT_ROOT'])));
 // echo dirname(__FILE__,2);
 // echo dirname($_SERVER['DOCUMENT_ROOT']);
-$htmlmode=１;//0:強制以特定名字 1:自適應 2:最終輸出模式(暫定)
+$htmlmode=1;//0:強制以特定名字 1:自適應 2:最終輸出模式(暫定)
 if($htmlmode==1 && dirname(__FILE__,2)=="/data1/adm/webteam.ccu.edu.tw/WWW/phaser"){
     define("root","/phaser");
 }else if($htmlmode==1 && dirname(__FILE__,2)=="/.CSDATA_NFS/home/under/u108/lyang108u/WWW/igem6"){
