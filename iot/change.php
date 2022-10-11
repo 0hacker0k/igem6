44a51f86bce8a83b43c2041ff3f743756960d5bb
0hacker0k <king9638k@gmail.com>
2022-10-11 22:17:48 +0800

    iot

--- iot/change.php
+++ iot/change.php
@@ -24,6 +24,13 @@ foreach($rs->fetchall() as $value){
         display: inline-block;
         vertical-align: top;
     }
+    button{
+        width:80%;
+        margin-left:10%;
+        margin-right:10%;
+        margin-top:1%;
+        margin-bottom:1%;
+    }
 </style>
 <form class="size_mid center" enctype="multipart/form-data" id="event_upload_form" method="post">
     <div style="padding:5%;width: 90%;margin: calc(5% - 1rem);border: 1rem solid;">
